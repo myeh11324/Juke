e8a82dd2f15cd9b3f7fd4caf1673fe46920686f3
Milly <owner@Millys-MacBook-Air.local>
2018-11-16 16:18:19 -0500

    both routes now working

--- server/api/album.js
+++ server/api/album.js
@@ -6,15 +6,20 @@ const {db, Album, Artist, Song} = require('../db');
 router.get('/', async (req, res, next) => {
   try {
     const allAlbums = await Album.findAll({include: [Artist]});
+    //const allAlbums = await Album.findAll();
     res.json(allAlbums);
   } catch(err) {
       next(err);
   }
 });
 
-router.get('/:albumid', async (req, res, next) => {
+router.get('/:albumId', async (req, res, next) => {
   try {
-
+    //For a single album, join info for artist and songs: 
+    const id = req.params.albumId
+    const singleAlbum = await Album.findById(id, {include: [Artist, Song]});
+    //const resultAlbum = await singleAlbum.findAll({include: [{Artist, include: [{Song}]}]});
+    res.json(singleAlbum);
   } catch (err) {
       next(err);
   }
